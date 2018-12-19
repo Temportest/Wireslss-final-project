dab0909e662dc090bdcc8b3d63797015c729b86a
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-12-19 13:27:25 +0800

    modify voice

--- js/index.js
+++ js/index.js
@@ -98,29 +98,23 @@
 // });
 
 function myclick(){
+  speak('請問要查詢哪裡的天氣');
+}
+ 
+function speak(str) {
   var msg = new SpeechSynthesisUtterance();
   var voices = window.speechSynthesis.getVoices();
-  msg.voice = voices[10]; // Note: some voices don't support altering params
-  msg.voiceURI = 'native';
+  // msg.voice = voices[10]; // Note: some voices don't support altering params
+  // msg.voiceURI = 'native';
   msg.volume = 1; // 0 to 1
-  msg.rate = 1.2; // 0.1 to 10
-  msg.pitch = 2; //0 to 2
-  msg.text = 'Sure. This code plays "Hello World" for you';
-  msg.lang = 'en-US';
+  msg.rate = 1; // 0.1 to 10
+  msg.pitch = 1; //0 to 2
+  msg.text = str;
+  msg.lang = 'zh';
 
-  msg.onend = function (e) {
-    var msg1 = new SpeechSynthesisUtterance('Now code plays audios for you');
-    msg1.voice = speechSynthesis.getVoices().filter(function (voice) { return voice.name == 'Whisper'; })[0];
-    msg1.pitch = 2; //0 to 2
-    msg1.rate = 1.2; //0 to 2
-    // start your audio loop.
-    speechSynthesis.speak(msg1);
-    console.log('Finished in ' + e.elapsedTime + ' seconds.');
-  };
   console.log('dsfds');
   speechSynthesis.speak(msg);
 }
-  
 
 
 
