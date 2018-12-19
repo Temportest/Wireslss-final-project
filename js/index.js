853d59384aa13cb0bdc234351e741e8961096ce0
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-12-19 13:57:03 +0800

    modify voice

--- js/index.js
+++ js/index.js
@@ -101,6 +101,8 @@ function myclick(){
   if ('speechSynthesis' in window) {
     // Synthesis support. Make your web apps talk!.
     speak('請問要查詢哪裡的天氣');
+  }else{
+    speak('can speak not');
   }
   
 }
