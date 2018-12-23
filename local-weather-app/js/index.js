73c026a30158791ec8093aff083ac8aa143e7c91
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-12-23 14:42:32 +0800

    modify get data array

--- local-weather-app/js/index.js
+++ local-weather-app/js/index.js
@@ -40,7 +40,7 @@ $(document).ready(function () {
 	function yourAddress() {
 		var googleApiCall = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=AIzaSyC8UY5L0pC6c3PaOZRcVr8u0R5cuxFC8qU&language=zh-TW`;
 		$.getJSON(googleApiCall, function (locationName) {
-			$(".locName").html(locationName.results[2].formatted_address);
+			$(".locName").html(locationName.results[1].formatted_address);
 			// console.log(locationName.results[2].formatted_address); (For checking the precision)
 		});
 	}
