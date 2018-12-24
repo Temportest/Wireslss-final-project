d44309ec47932ebb9ca5e96b089604c58bf14316
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-12-24 09:35:33 +0800

    add test

--- server/index.js
+++ server/index.js
@@ -20,8 +20,10 @@ app.get('/', function (request, response) {
   response.render('index.html', { env: envName });
 });
 
-app.listen(app.get('port'), function () {
-  console.log("Node app running at localhost:" + app.get('port'));
-});
+if (!module.parent) {
+  app.listen(app.get('port'), function () {
+    console.log("Node app running at localhost:" + app.get('port'));
+  });
+}
 
 module.exports = app
